e7e0a5093776989ae3ea19beddb71af1fd973e2f
angelgracecy <angelgracecy@gmail.com>
2024-06-25 23:41:33 -0700

    weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,5 @@
 def get_weather_advice():
-    
-    weather = input("What’s the weather like today? (sunny/rainy/cold): ").strip().lower()
+        weather = input("What’s the weather like today? (sunny/rainy/cold): ")
     
     if weather == "sunny":
         print("Wear a t-shirt and sunglasses.")
