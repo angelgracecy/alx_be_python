757f6150cc34c03754e57c44fd386bfb2892143c
angelgracecy <angelgracecy@gmail.com>
2024-06-27 02:38:24 -0700

    calculatop

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     number = number * i
-    print(f"{number} * i")
+    print(f"{number} * {i} = {number}")
